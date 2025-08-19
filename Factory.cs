790017de31e4917c0dcce43c2b807a5b4844ad24
Aston-Bratley <006092@cac.qld.edu.au>
2025-08-19 13:31:33 +1000

    Create Factory.cs

---
+++ Factory.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace UIKit
+{
+    public static class UIFactory
+    {
+        // Create a root Canvas and wrapper UIElement and optionally parent it under HUDManager's HUD canvas
+        public static UIElement CreateRoot(string name = "UIRoot", Transform parent = null)
+        {
+            var go = new GameObject(name, typeof(RectTransform), typeof(Canvas), typeof(CanvasScaler), typeof(UnityEngine.UI.GraphicRaycaster));
+            var canvas = go.GetComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            var scaler = go.GetComponent<CanvasScaler>();
+            scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            scaler.referenceResolution = new Vector2(1920, 1080);
+            if (parent != null) go.transform.SetParent(parent, false);
+
+            var wrapper = new GameObject("RootWrapper", typeof(UIElement));
+            wrapper.transform.SetParent(go.transform, false);
+            var element = wrapper.GetComponent<UIElement>();
+            element.ApplyStyle(); // ensure defaults take effect
+            return element;
+        }
+
+        // Attach a UI root underneath the in-game HUD canvas (HUDElements[2])
+        public static UIElement CreateHudRoot(string name = "HudUI")
+        {
+            var hud = UnityEngine.Object.FindObjectOfType<HUDManager>();
+            Transform parent = null;
+            if (hud != null && hud.HUDElements != null && hud.HUDElements.Length > 2)
+            {
+                parent = hud.HUDElements[2].canvasGroup.transform.parent;
+            }
+
+            return CreateRoot(name, parent);
+        }
+    }
+}
